cc1c54cfaafd47c26cf000fa3c30f8211113b9b3
Emil Nuretdinov <emilnuretdinov@MacBook-Air-Emil.local>
2022-06-23 21:00:45 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:foreveremil/vue3-training.git master:gh-pages
+git push -f git@github.com:ForeverEmil/vue3-training.git master:gh-pages
 
 cd -
